9f564b6ca2c4a323ac72b3e947f8dc9b36824403
Christian Riano <christianrv.dev@gmail.com>
2024-01-16 10:49:56 -0500

    feature: Create route for google sign-in

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const { check } = require('express-validator')
 
-const { login } = require('../controllers/auth.controller')
+const { login, googleSignIn } = require('../controllers/auth.controller')
 const { checkFields } = require('../middlewares/fields-validator')
 
 router.post(
@@ -12,6 +12,12 @@ router.post(
     checkFields
   ],
   login
+)
+
+router.post(
+  '/google',
+  [check('id_token', 'The token is mandatory').not().isEmpty(), checkFields],
+  googleSignIn
 )
 
 module.exports = router
