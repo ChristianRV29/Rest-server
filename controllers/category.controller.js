9c5c1ce5cbcc96ec3cd9c68858c98cccf1ade414
Christian Riano <christianrv.dev@gmail.com>
2024-01-28 09:03:03 -0500

    feature: Set up category controller
    Get category by id
    
    Getall categories

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -40,13 +40,74 @@ const createCategory = async (req = request, res = response) => {
 }
 
 const getCategories = async (req = request, res = response) => {
-  res.status(200).json({
-    success: true,
-    message: 'Nice'
-  })
+  try {
+    const { from = 0, limit = 10 } = req.query
+
+    const query = { status: true }
+
+    const allCategories = Category.find(query)
+      .limit(Number(limit))
+      .skip(Number(from))
+
+    const totalCategories = Category.countDocuments(query)
+
+    const [total, categories] = await Promise.all([
+      totalCategories,
+      allCategories
+    ])
+
+    res.status(200).json({
+      success: true,
+      message: 'The categories were brought!',
+      data: {
+        count: categories.length,
+        total,
+        categories
+      }
+    })
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err
+    })
+  }
+}
+
+const getCategoryById = async (req = request, res = response) => {
+  try {
+    const { id } = req.params
+
+    const category = await Category.findById(id)
+
+    if (!category) {
+      return res.status(401).json({
+        success: false,
+        error: 'The category does not exist'
+      })
+    } else if (!category.status) {
+      return res.status(401).json({
+        success: false,
+        error: 'The category is not active - please talk to an administrator'
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      message: 'The category was brought!',
+      data: {
+        category
+      }
+    })
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err
+    })
+  }
 }
 
 module.exports = {
   createCategory,
-  getCategories
+  getCategories,
+  getCategoryById
 }
