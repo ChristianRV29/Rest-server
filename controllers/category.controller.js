4a16882044f7dd4230558ba608b06d8cf7f0ce45
Christian Riano <christianrv.dev@gmail.com>
2024-01-28 08:30:40 -0500

    feature: Create category's creation controller
    Check if category already exists
    
    Save category into data base

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,6 +1,43 @@
 const { request, response } = require('express')
 
-const createCategory = async (req = request, res = response) => {}
+const { Category } = require('../models')
+
+const createCategory = async (req = request, res = response) => {
+  try {
+    const { name } = req.body
+
+    const category = await Category.findOne({ name: name.toUpperCase() })
+
+    if (category) {
+      return res.status(401).json({
+        success: false,
+        error: `The category ${name} already exists`
+      })
+    }
+
+    const data = {
+      name: name.toUpperCase(),
+      user_id: req.user._id
+    }
+
+    const newCategory = await Category(data)
+
+    await newCategory.save()
+
+    res.status(200).json({
+      success: true,
+      message: 'The category was created!',
+      data: {
+        category: newCategory
+      }
+    })
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err
+    })
+  }
+}
 
 const getCategories = async (req = request, res = response) => {
   res.status(200).json({
