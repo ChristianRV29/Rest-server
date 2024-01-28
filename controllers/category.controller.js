180506f2ca2810ef6f8b7eccbfb8f566c084ac65
Christian Riano <christianrv.dev@gmail.com>
2024-01-28 09:56:38 -0500

    chore: Update category controller
    Create functions for updating and deleting categories

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -9,7 +9,7 @@ const createCategory = async (req = request, res = response) => {
     const category = await Category.findOne({ name: name.toUpperCase() })
 
     if (category) {
-      return res.status(401).json({
+      return res.status(400).json({
         success: false,
         error: `The category ${name} already exists`
       })
@@ -17,7 +17,7 @@ const createCategory = async (req = request, res = response) => {
 
     const data = {
       name: name.toUpperCase(),
-      user_id: req.user._id
+      created_by: req.user._id
     }
 
     const newCategory = await Category(data)
@@ -46,6 +46,7 @@ const getCategories = async (req = request, res = response) => {
     const query = { status: true }
 
     const allCategories = Category.find(query)
+      .populate('created_by')
       .limit(Number(limit))
       .skip(Number(from))
 
@@ -77,15 +78,10 @@ const getCategoryById = async (req = request, res = response) => {
   try {
     const { id } = req.params
 
-    const category = await Category.findById(id)
+    const category = await Category.findById(id).populate('created_by')
 
-    if (!category) {
-      return res.status(401).json({
-        success: false,
-        error: 'The category does not exist'
-      })
-    } else if (!category.status) {
-      return res.status(401).json({
+    if (category && !category.status) {
+      return res.status(400).json({
         success: false,
         error: 'The category is not active - please talk to an administrator'
       })
@@ -106,8 +102,53 @@ const getCategoryById = async (req = request, res = response) => {
   }
 }
 
+const updateCategory = async (req = request, res = response) => {
+  try {
+    const { id } = req.params
+    const name = req.body.name.toUpperCase()
+
+    const category = await Category.findByIdAndUpdate(id, { name })
+
+    res.status(200).json({
+      success: true,
+      message: 'The category was updated!',
+      data: {
+        category
+      }
+    })
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err
+    })
+  }
+}
+
+const deleteCategory = async (req = request, res = response) => {
+  try {
+    const { id } = req.params
+
+    const category = await Category.findByIdAndUpdate(id, { status: false })
+
+    res.status(200).json({
+      success: true,
+      message: 'The category was deleted!',
+      data: {
+        category
+      }
+    })
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err
+    })
+  }
+}
+
 module.exports = {
   createCategory,
+  deleteCategory,
   getCategories,
-  getCategoryById
+  getCategoryById,
+  updateCategory
 }
