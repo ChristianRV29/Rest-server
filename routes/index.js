32d1a22180e9c04c146de8c261e3d2d352f5debe
Christian Riano <christianrv.dev@gmail.com>
2024-02-03 09:08:59 -0500

    feature: Configure searches routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const Auth = require('./auth.routes')
 const Categories = require('./category.routes')
-const Users = require('./user.routes')
 const Products = require('./product.routes')
+const Searches = require('./search.routes')
+const Users = require('./user.routes')
 
 module.exports = {
   Auth,
   Categories,
   Products,
+  Searches,
   Users
 }
