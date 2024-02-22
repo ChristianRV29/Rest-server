0fe0d31cefdef53e1ff765f1f74df6f609c8a698
Christian Riano <christianrv.dev@gmail.com>
2024-02-22 14:26:31 -0500

    refactor: Update code to handle different collections

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -1,6 +1,8 @@
 const { request, response } = require('express')
 const { organizeFiles } = require('../helpers')
 
+const { User, Product } = require('../models')
+
 const uploadFile = async (req = request, res = response) => {
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).json({
@@ -43,26 +45,50 @@ const updateUserImage = async (req = request, res = response) => {
     switch (collection) {
       case 'users':
         model = await User.findById(id)
-
-        if (!model) {
-          return res.status(404).json({
-            success: false,
-            message: 'User not found'
-          })
-        }
-
-        model.img = req.body.img
-
         break
+
       case 'products':
+        model = await Product.findById(id)
         break
 
       default:
-        return res.status(500).json({
+        return res.status(401).json({
           success: false,
-          message: 'Collection is not valid'
+          error: 'The collection is not valid'
         })
     }
+
+    if (!model) {
+      return res.status(404).json({
+        success: false,
+        message: 'The id does not exist'
+      })
+    } else if (!model.status) {
+      return res.status(400).json({
+        success: false,
+        message: 'The id is not active'
+      })
+    }
+
+    const { error, data } = await organizeFiles(req.files, collection)
+
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        message: error
+      })
+    }
+
+    model.img = data
+    await model.save()
+
+    return res.status(200).json({
+      data: {
+        model
+      },
+      message: 'Image updated successfully',
+      success: true
+    })
   } catch (err) {
     res.status(500).json({
       error: err,
