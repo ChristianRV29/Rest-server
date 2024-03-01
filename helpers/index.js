35e69939f8f272ab40a0285d6437c52986b0a84a
Christian Riano <christianrv.dev@gmail.com>
2024-03-01 09:37:49 -0500

    chore: create a random password generator

--- helpers/index.js
+++ helpers/index.js
@@ -2,10 +2,12 @@ const dbValidators = require('./db-validators')
 const google = require('./google-verify')
 const jwt = require('./jwt')
 const uploadFiles = require('./upload-files')
+const password = require('./password')
 
 module.exports = {
   ...dbValidators,
   ...google,
   ...jwt,
+  ...password,
   ...uploadFiles
 }
