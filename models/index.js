b3575e96442c0c4647c268933f20264624fe0aed
Christian Riano <christianrv.dev@gmail.com>
2024-03-05 09:19:33 -0500

    feature: create a new model for chat management

--- models/index.js
+++ models/index.js
@@ -3,9 +3,11 @@ const Role = require('./role')
 const Server = require('./server')
 const User = require('./user')
 const Product = require('./product')
+const Chat = require('./chat')
 
 module.exports = {
   Category,
+  Chat,
   Product,
   Role,
   Server,
